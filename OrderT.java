f8cf1ac1d6fca23f6763a28d4e270c7425f12b81
VISHWANATHAN A <114131407+VishwanathanA@users.noreply.github.com>
2024-08-08 09:26:45 +0530

    Add files via upload

---
+++ OrderT.java
@@ -0,0 +1,56 @@
+class TreeNode{
+    int val;
+    TreeNode left,right;
+    TreeNode(int val){
+        this.val=val;
+        left=right=null;
+    }
+}
+public class OrderT{
+    public static void inOrder(TreeNode root){
+        if(root==null)
+        {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val+" ");
+        inOrder(root.right);
+}
+public static void preOrder(TreeNode root){
+    if(root==null)
+    {
+        return;
+    }
+    System.out.print(root.val+" ");
+    preOrder(root.left);
+    preOrder(root.right);
+}
+public static void postOrder(TreeNode root){
+    if(root==null)
+    {
+        return;
+    }
+    postOrder(root.left);
+    postOrder(root.right);
+    System.out.print(root.val+" ");
+}
+public static void main(String[] args)
+{
+    TreeNode root=new TreeNode(4);
+    root.left=new TreeNode(2);
+    root.right=new TreeNode(5);
+    root.left.left=new TreeNode(3);
+    root.left.left.right=new TreeNode(9);
+    root.left.left.right.left=new TreeNode(1);
+    root.right.left=new TreeNode(7);
+    root.right.right=new TreeNode(6);
+    root.right.right.left=new TreeNode(8);
+    OrderT tree=new OrderT();
+    System.out.print("Inorder Traversal : ");
+    tree.inOrder(root);
+    System.out.print("\npreorder Traversal : ");
+    tree.preOrder(root);
+    System.out.print("\npostorder Traversal : ");
+    tree.postOrder(root);
+}
+}
